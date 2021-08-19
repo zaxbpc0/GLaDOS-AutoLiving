fa0411d75bc1c8797a06a0ece6926d1d328eeea2
zaxbpc0 <63100307+zaxbpc0@users.noreply.github.com>
2021-08-20 00:36:31 +0800

    Add files via upload

---
+++ checkin.py
@@ -0,0 +1,51 @@
+import requests,json,os
+
+# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
+sever = os.environ["SERVE"]
+# 填写server酱sckey,不开启server酱则不用填
+sckey = os.environ["SCKEY"]
+#'SCU89402Tf98b7f01ca3394*********************************'
+# 填入glados账号对应cookie
+cookie = os.environ["COOKIE"]
+#'__cfduid=d3459ec306384ca67a65170f8e2a5bd************; _ga=GA1.2.766373509.1593*****72; _gid=GA1.2.1338236108.***********72; koa:sess=eyJ1c2VySW*********************aXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdl****0=; koa:sess.sig=6qG8SyMh*****LBc9yRviaPvI'
+
+
+
+
+def start():
+    
+    url= "https://glados.rocks/api/user/checkin"
+    url2= "https://glados.rocks/api/user/status"
+    origin = "https://glados.rocks"
+    referer = "https://glados.rocks/console/checkin"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+    payload={
+        'token': 'glados_network'
+    }
+    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
+    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+   # print(res)
+
+    if 'message' in checkin.text:
+        mess = checkin.json()['message']
+        if mess == '\u6ca1\u6709\u6743\u9650':
+            requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+        time = state.json()['data']['leftDays']
+        time = time.split('.')[0]
+        #print(time)
+        notice(time,sckey,sever,mess)
+
+        
+def notice(time,sckey,sever,mess):
+    if sever == 'on':
+        requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+    else:
+        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=通知没打开')
+        
+def main_handler(event, context):
+  return start()
+
+if __name__ == '__main__':
+    start()
+
+    
---
+++ 云函数py.py
@@ -0,0 +1,43 @@
+import requests ,os,json
+# server酱开关，填0不开启(默认)，填2同时开启cookie失效通知和签到成功通知
+sever = 'on'
+# 填写server酱sckey,不开启server酱则不用填（自己更改）
+sckey = 'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1a****************'
+# 填入glados账号对应cookie
+cookie = '__cfduid=d825014ac33b402b3f**************'
+referer = 'https://glados.rocks/console/checkin'
+
+def start():  
+    url= "https://glados.rocks/api/user/checkin"
+    url2= "https://glados.rocks/api/user/status"
+    origin = "https://glados.rocks"
+    referer = "https://glados.rocks/console/checkin"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+    payload={
+        'token': 'glados_network'
+    }
+    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
+    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+   # print(res)
+
+    if 'message' in checkin.text:
+        mess = checkin.json()['message']
+        if mess == '\u6ca1\u6709\u6743\u9650':
+            requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+        time = state.json()['data']['leftDays']
+        time = time.split('.')[0]
+        #print(time)
+        notice(time,sckey,sever,mess)
+
+        
+def notice(time,sckey,sever,mess):
+    if sever == 'on':
+        requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+    else:
+        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=通知没打开')
+        
+def main_handler(event, context):
+  return start()
+
+if __name__ == '__main__':
+    start()
