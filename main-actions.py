7695f2300038ab051bf7a634982f77604f00c640
zaxbpc0 <63100307+zaxbpc0@users.noreply.github.com>
2021-08-20 11:37:05 +0800

    Update main-actions.py

--- main-actions.py
+++ main-actions.py
@@ -1,17 +1,10 @@
 import requests,json,os
 
-
 PUSHPLUSTOKEN = os.environ["PUSHPLUS_TOKEN"]
-
+qq = os.environ["QQ"]
+qmsg_key = os.environ["QMSG_KEY"]
 cookie = os.environ["COOKIE"]
-#'__cfduid=d3459ec306384ca67a65170f8e2a5bd************; _ga=GA1.2.766373509.1593*****72; _gid=GA1.2.1338236108.***********72; koa:sess=eyJ1c2VySW*********************aXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdl****0=; koa:sess.sig=6qG8SyMh*****LBc9yRviaPvI'
-
-desp = ''  # 空值
-
-def log(info: str):
-    print(info)
-    global desp
-    desp = desp + info + '\n'
+    
     
 def pushplus(token, title, content):
     url = 'http://www.pushplus.plus/send'
@@ -23,14 +16,27 @@ def pushplus(token, title, content):
     body = json.dumps(data).encode(encoding='utf-8')
     headers = {'Content-Type': 'application/json'}
     rs = requests.post(url, data=body, headers=headers).json()
-    print(rs)
     if int(rs["code"] / 100) != 2:
         print('PushPlus 推送失败')
     else:
         print('PushPlus 推送成功')
+        
+        
+def qmsg(qmsg_key, qq, style): # style: msg,json,xml
+    # urlg='https://qmsg.zendee.cn/group/' + qmsg_key  #群消息推送接口
+    urls='https://qmsg.zendee.cn/send/' + qmsg_key   #私聊消息推送接口
+    data = {
+        "qq": qq,
+        "msg": style
+    }
+    rs = requests.post(urls,data=data).json()
+    if int(rs["code"] / 100) != 0:
+        print('Qmsg酱 推送失败')
+    else:
+        print('Qmsg酱 推送成功')
 
-def start():
-    
+
+def start(): 
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     origin = "https://glados.rocks"
@@ -46,12 +52,13 @@ def start():
     if 'message' in checkin.text:
         mess = checkin.json()['message']
         if mess == '\u6ca1\u6709\u6743\u9650':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+            #requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+            print('cookie过期')
+            return 'cookie过期'
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         #print(time)
-        #notice(time,sckey,sever,mess)
-        pushplus(PUSHPLUSTOKEN, 'GLaDOS日志', mess)
+    return mess
 
         
 def notice(time,sckey,sever,mess):
@@ -64,6 +71,6 @@ def main_handler(event, context):
   return start()
 
 if __name__ == '__main__':
-    start()
-   
-
+    mes = start()
+    qmsg(qmsg_key, qq, 'GLaDOS日志:\n' + mes)
+    pushplus(PUSHPLUSTOKEN, 'GLaDOS日志', '123' + mes)
