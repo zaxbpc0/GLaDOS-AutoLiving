f372783682d766db8f43545b6bf76a35a7dc7f67
zaxbpc0 <63100307+zaxbpc0@users.noreply.github.com>
2021-08-20 11:22:03 +0800

    Update main-actions.py

--- main-actions.py
+++ main-actions.py
@@ -1,8 +1,6 @@
 import requests,json,os
 
 PUSHPLUSTOKEN = os.environ["PUSHPLUS_TOKEN"]
-qq = os.environ["QQ"]
-qmsg_key = os.environ["QMSG_KEY"]
 cookie = os.environ["COOKIE"]
 
 desp = ''  # 空值
@@ -26,25 +24,9 @@ def pushplus(token, title, content):
         print('PushPlus 推送失败')
     else:
         print('PushPlus 推送成功')
-        
-        
-def qmsg(qmsg_key, qq, style): # style: msg,json,xml
-    # urlg='https://qmsg.zendee.cn/group/' + qmsg_key  #群消息推送接口
-    urls='https://qmsg.zendee.cn/send/' + qmsg_key   #私聊消息推送接口
-    data = {
-        "qq": qq,
-        "msg": style
-    }
-    #body = json.dumps(data).encode(encoding='utf-8')
-    #headers = {'Content-Type': 'application/json'}
-    rs = requests.post(urls,data=data).json()
-    if int(rs["code"] / 100) != 0:
-        print('Qmsg酱 推送失败')
-    else:
-        print('Qmsg酱 推送成功')
 
-
-def start(): 
+def start():
+    
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     origin = "https://glados.rocks"
@@ -61,15 +43,11 @@ def start():
         mess = checkin.json()['message']
         if mess == '\u6ca1\u6709\u6743\u9650':
             requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
-            #print('cookie过期')
-            #return 'cookie过期'
-            #exit(1)
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         #print(time)
         #notice(time,sckey,sever,mess)
         pushplus(PUSHPLUSTOKEN, 'GLaDOS日志', mess)
-    #return mess
 
         
 def notice(time,sckey,sever,mess):
@@ -83,8 +61,3 @@ def main_handler(event, context):
 
 if __name__ == '__main__':
     start()
-    #pushplus(PUSHPLUSTOKEN, 'GLaDOS日志', 'mes')
-    #qmsg(qmsg_key, qq, 'GLaDOS日志:\n'+mes)
-   
-
-    
